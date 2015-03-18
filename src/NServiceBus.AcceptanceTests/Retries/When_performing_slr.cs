052f20fd585be2336e0194e7241e6ac17bb2ca91
Andreas Öhlund <andreas.ohlund@particular.net>
2015-03-18 14:35:13 +0100

    Adjuted the mutators to the new context

--- src/NServiceBus.AcceptanceTests/Retries/When_performing_slr.cs
+++ src/NServiceBus.AcceptanceTests/Retries/When_performing_slr.cs
@@ -33,7 +33,7 @@
                     });
             }
    
-            class BodyMutator : IMutateTransportMessages, INeedInitialization
+            class BodyMutator : IMutateOutgoingPhysicalContext,IMutateIncomingTransportMessages, INeedInitialization
             {
                 public Context Context { get; set; }
 
@@ -60,12 +60,16 @@
 
                 public void MutateOutgoing(LogicalMessage logicalMessage, TransportMessage transportMessage)
                 {
-                    transportMessage.Body[0]--;
                 }
 
                 public void Customize(BusConfiguration configuration)
                 {
                     configuration.RegisterComponents(c => c.ConfigureComponent<BodyMutator>(DependencyLifecycle.InstancePerCall));
+                }
+
+                public void MutateOutgoing(OutgoingPhysicalMutatorContext context)
+                {
+                    context.Body[0]--;
                 }
             }
 
